0da033c3a8137b6d1edf712883678ade4881edc4
jaspertham <31091548+jaspetham@users.noreply.github.com>
2020-12-09 15:24:14 +0800

    added banner

--- src/Banner/Banner.js
+++ src/Banner/Banner.js
@@ -1,10 +1,65 @@
-import React from 'react'
-
+import React from 'react';
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+import {Col, Container, Image, Row } from 'react-bootstrap';
+import './Banner.css';
+import BannerImage from '../assets/banner.png';
+import CaroImage from '../assets/caro-img.png';
+import CaroBanner from '../assets/caro-banner.png';
+import CaroElement from '../Reuse/CaroElement';
 function Banner() {
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+    };
+
     return (
-        <div>
-            
-        </div>
+        <section id="banner" className="paddingY">
+            <Container fluid className="paddingX">
+                <Row>
+                    <Col md={9}>
+                        <Image fluid src={BannerImage} className="banner-img"/>
+                    </Col>
+                    <Col md={3} className="my-auto">
+                        <Slider {...settings}>
+                            <CaroElement
+                                bigImgSrc={CaroBanner}
+                                smallImgSrc={CaroImage}
+                                author="Ana Chiew"
+                                atnLevel="High"
+                                title="Virtual Shopping! From beauty cream to oils and skin care. Bid now!"
+                                liveUserNum={321}
+                                liveDuration="5:02:09"
+                            />
+                            <CaroElement
+                                bigImgSrc={CaroBanner}
+                                smallImgSrc={CaroImage}
+                                author="Ana Chiew"
+                                atnLevel="High"
+                                title="Virtual Shopping! From beauty cream to oils and skin care. Bid now!"
+                                liveUserNum={3111}
+                                liveDuration="5:02:09"
+                            />
+                            <CaroElement
+                                bigImgSrc={CaroBanner}
+                                smallImgSrc={CaroImage}
+                                author="Ana Chiew"
+                                atnLevel="High"
+                                title="Virtual Shopping! From beauty cream to oils and skin care. Bid now!"
+                                liveUserNum={301}
+                                liveDuration="5:02:09"
+                            />
+                        </Slider>
+                      
+
+                    </Col>
+                </Row>
+            </Container>
+        </section>
     )
 }
 
---
+++ src/Reuse/CaroElement.js
@@ -0,0 +1,33 @@
+import { faUserCircle } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from 'react';
+import { Button, Card, Col, Image, Row } from 'react-bootstrap';
+import './CaroElement.css';
+
+function CaroElement(props) {
+    return (
+        <Card>
+            <Card.Img variant="top" src={props.bigImgSrc} />
+            <Card.Body className="special-card">
+                <Row>
+                    <Col md={5} className="p-0">
+                        <Image src={props.smallImgSrc} fluid/>
+                    </Col>
+                    <Col md={7} className="p-0">
+                        <div className="caro-right">
+                            <Button variant="live" className="my-2">
+                                Live <FontAwesomeIcon className="mx-1"icon={faUserCircle}/> {props.liveUserNum}
+                            </Button>
+                            <span className="pl-4 text-white">{props.liveDuration}</span>
+                            <p>{props.title}</p>
+                            <small>by {props.author}</small>
+                            <Button variant="atn" className="my-2">Atn {props.atnLevel}</Button>
+                        </div>
+                    </Col>
+                </Row>
+            </Card.Body>
+        </Card>
+    )
+}
+
+export default CaroElement
